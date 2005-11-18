dacca562b368b8401ff5578083e4a4ff19396925
ambrmi09 <ambrmi09>
2005-11-18 11:26:09 +0000

    Messing with time. Considering to re-design so that resolution better fits with support for POSIX 1003.1c (nS potential resolution)

--- src/time.c
+++ src/time.c
@@ -6,24 +6,45 @@
 //  POSIX CLOCKS_PER_SEC is preset to 1*10e6. How many clock_ticks is there 
 //  (or would there be) in a sys_tick.
 
-#define CLKS_PER_TCK 1000 
+#define TICK_PER_CLK 1000  //<! How much a tick is advanced on
+//each interrupt
 
 
-
-//! This is the internal data of the timer system. Observe that accessing this 
-//  variable is not thread/interrupt safe. This needs to be revised. 
-//
-//  @note Also very important - always include the memory type "language 
-//  extention" when declaring external cariables like this.
-extern unsigned long idata sys_tick;
-extern unsigned long idata sys_mackey;
-
-
+#include "tk_tick.h"  //internal macros for tick handliing
 
 //return (clock_t)(-1);
 
+
+/**
+
+@ingroup POSIX
+        
+@brief returns time since startup in 1 millionth of a second (uS)
+
+The formal POSIX clock is somewhat different. There the time is relative
+to since the calling <b>process</b> was started. On most small embedded
+systems, including those using this kernel there is only one process and
+that is the whole binary it self. I.e. the meaning of a process is
+the whole program and our clock funtion relates to time since startup
+(reset).
+
+@note Upper limit of for how long up-time can be determined is
+dependant on the definition of clock_t. clock_t <b>should</b> be a
+at least a 32-bit value. That means that this function will return the
+same result approximately every 72 minutes.
+
+@see clock_t
+
+References:<br>
+http://www.gnu.org/software/libc/manual/html_mono/libc.html#CPU%20Time
+   
+@return time since start in uS
+   
+   
+*/
+
 clock_t clock(){
-   return CLKS_PER_TCK * sys_tick;
+   return TICK_PER_CLK * sys_tick;
 }
 
 
