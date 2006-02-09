6cc7e61770d7d839c2dd115e9dc984d35fd4612b
ambrmi09 <ambrmi09>
2006-02-09 22:40:39 +0000

    Initial creation of ANSI re-implementation module

---
+++ src/tk_ansi.h
@@ -0,0 +1,199 @@
+/*!
+@file
+@ingroup kernel_reimpl_ansi
+
+@brief ANSI search functions reimplemented
+
+This is the common headerfile for all ANSI re-implementations
+
+For in-depth discussions about re-implementing ANSI functions, see \ref
+kernel_reimpl_ansi
+
+@see kernel_reimpl_ansi
+
+*/
+
+#ifndef TK_ANSI_H
+#define TK_ANSI_H
+
+//#include <stdio.h>
+#include <stdlib.h>
+#ifndef COMPARISON_FN_T 
+/*!
+A comparison funtion definition that ANSI search & sort funtions use.
+It's return value is supposed to have the same meaning as for strcmp
+
+@return \f$ = \cases{ 0, & if L = R \cr <0 , & if L < R \cr >0, & if L > R } \f$
+
+
+@return  0 if eq, 
+@return <0 if L<R 
+@return >0 if L>R 
+
+GNU reference: @see http://www.gnu.org/software/libc/manual/html_mono/libc.html#Comparison%20Functions
+*/
+typedef int comparison_fn_t (  
+   const void *L,  //!< <em>"Leftmost"</em> element to compare with
+   const void *R   //!< <em>"Rightmost"</em> element to compare with
+);
+
+#endif
+
+
+/*!
+@name CONDITIONALS
+
+These macro's define whether a certain re-implemented ANSI function
+should be part of the TinKer binary or not.
+
+*/
+
+//@{
+
+#define TK_NEEDS_QSORT
+#define TK_NEEDS_BSEARCH
+#define TK_NEEDS_PRINTF
+
+//@}
+
+/** @defgroup kernel_reimpl_ansi ANSI functions reimplemented 
+@ingroup CODEMOD 
+@brief Files containing re-emplemented ANSI functions
+
+The files in this Doxygen "module" are involved one way or the other
+with reimplemtation of various ANSI functions, defines e.t.a. The
+"stuff" we're talking about is what is normally part of stdlib (i.e.
+what stdlib.h defines).
+
+@section Intro 
+In case you do have a corresponding function, you don't
+want to have both. Because of that each function is conditionally
+compiled and you have to explicitly set each definition. This way when
+you port for a new target (assuming no conditionals have been set by you
+yet), you will know if a function is supported or not since the linker
+will produce link errors for functions not found.
+
+   
+@section missing_functions Simply missing functions
+Typically a tool-chain comes with only partial of the stdlib. Here
+comes an example of functions <b>NOT</b> included in such a typical
+tool-chain (Keil). 
+
+@see http://www.keil.com/support/man/docs/c166/c166_xa_librarydif.htm
+
+@note If a function in the list below is colored blue, that means that
+the specific function is both implemented AND included in this certain
+build of TinKer. Use this as a fast way to determine which version
+of a certain function you're useing (in case of uncertainty).
+   
+The following ANSI Standard library routines are not included in the C166 library:
+
+abort
+asctime
+atexit
+bsearch
+clearerr
+clock
+ctime
+difftime
+div
+exit
+fclose
+feof
+ferror
+fflush
+fgetc
+fgetpos
+fgets  
+fmod
+fopen
+fprintf
+fputc
+fputs
+fread
+freopen
+frexp
+fscanf
+fseek
+fsetpos
+ftell
+fwrite
+getc
+getenv
+gmtime
+ldexp  
+ldiv
+localeconv
+localtime
+mblen
+mbstowcs
+mbtowc
+mktime
+perror
+putc
+\ref                       qsort
+raise
+remove
+rename
+rewind
+setbuf
+setlocale
+setvbuf  
+signal
+strcoll
+strerror
+strftime
+strtok
+strxfrm
+system
+time
+tmpfile
+tmpnam
+ungetc
+vfprintf
+wcstombs
+wctomb  
+
+The following ANSI Standard library routines is probably part of you
+tool-chain, but <em>you most likely</em> need replace with TinKer
+equivalents:
+
+\ref                       printf
+\ref                       putchar
+
+@section other_reasons Other reasons
+In some cases there exist other reasons for why you would want to use
+TinKers re-implemented version of a certain ANSI function:
+
+- Re-entrance, your tool suppliers version is not re-entrant. Since
+threads can preempt another thread currently in any function (including
+stdlib) and make another call to the same function, the result of the
+first one can get corrupted (if the function wasn't designed considering
+re-entrance). This is however in most embedded tool-chain not an issue,
+but you should really check.
+
+- Thread safety, your tool suppliers version is not thread safe. Part of
+the function must never be preempted (or interrupted), but you supplier
+never considered that case. File operations are such a case (including
+terminal I/O such as the common printf).
+
+- Memory issues, your tool suppliers version is hogging memory or 
+allocating from regions that you can't allow (or don't support) e.t.a.
+
+- Optimizations, you might need a different behaviour (speed, temporal
+determinism e.t.a.)
+
+- And many other reasons that I cant remember right now...
+
+*/
+
+#endif
+
+/*! 
+ * @addtogroup CVSLOG CVSLOG
+ *  $Log: tk_ansi.h,v $
+ *  Revision 1.1  2006-02-09 22:40:39  ambrmi09
+ *  Initial creation of ANSI re-implementation module
+ *
+ *  
+ *******************************************************************/
---
+++ src/tk_ansi_search.c
@@ -0,0 +1,122 @@
+/*!
+@file
+@ingroup kernel_reimpl_ansi
+
+@brief ANSI search & sort functions reimplemented
+
+The functions in this file are all reimplementations of ANSI functions
+for searching and sorting, that TinKer relies on but that are commonly
+missing from many embedded toolchain suppliers.
+
+*/
+
+#include "tk_ansi.h"
+
+
+#if defined(TK_NEEDS_QSORT)
+/*!
+To sort an array using an arbitrary comparison function, use the qsort
+function. The prototype for this function is in stdlib.h.
+
+Function: void qsort (void *array, size_t count, size_t size,
+comparison_fn_t compare)
+
+The qsort function sorts the array <em>array</em>. The array
+contains <em>count</em> elements, each of which is of size
+<em>size</em>.
+
+The <em>compare</em> function is used to perform the comparison on the
+array elements. This function is called with two pointer arguments and should
+return an integer less than, equal to, or greater than zero
+corresponding to whether its first argument is considered less than,
+equal to, or greater than its second argument.
+
+
+@note Warning: If two objects compare as equal, their order after sorting is
+unpredictable. That is to say, the sorting is not stable. This can make
+a difference when the comparison considers only part of the elements.
+Two elements with the same sort key may differ in other respects.
+
+If you want the effect of a stable sort, you can get this result by
+writing the comparison function so that, lacking other reason
+distinguish between two elements, it compares them by their addresses.
+Note that doing this may make the sorting algorithm less efficient, so
+do it only if necessary.
+
+GNU reference: @see http://www.gnu.org/software/libc/manual/html_mono/libc.html#Array%20Sort%20Function
+
+*/
+void qsort (
+   void *array,             //!< The array to be sorted
+   size_t count,            //!< Number of elements in the array
+   size_t size,             //!< Size of each element
+   comparison_fn_t compare  //!< Comparison function 
+){
+}
+#endif
+
+#if defined(TK_NEEDS_BSEARCH)
+/*!
+To search a sorted array for an element matching the key, use the
+bsearch function. The prototype for this function is in the header file
+stdlib.h.
+
+void * bsearch (const void *key, const void *array, size_t count, size_t
+size, comparison_fn_t compare) 
+
+The bsearch function searches the sorted array <em>array</em> for an
+object that is equivalent to <em>key</em>. The array contains
+<em>count</em> elements, each of which is of size <em>size</em> bytes.
+
+The compare function is used to perform the comparison. This function is
+called with two pointer arguments and should return an integer less
+than, equal to, or greater than zero corresponding to whether its first
+argument is considered less than, equal to, or greater than its second
+argument. <i>The elements of the array must already be sorted in
+ascending order according to this comparison function.</i>
+
+
+@return The return value is a pointer to the matching array element, or
+a null pointer if no match is found. If the array contains more than one
+element that matches, the one that is returned is unspecified. 
+
+This function derives its name from the fact that it is implemented
+using the binary search algorithm. 
+
+GNU reference: @see http://www.gnu.org/software/libc/manual/html_mono/libc.html#Array%20Search%20Function
+*/
+
+void * bsearch (
+   const void *key,         //!< Serch for this key
+   const void *array,       //!< Sorted array to search for key
+   size_t count,            //!< Number of elements in the array
+   size_t size,             //!< Size of each element
+   comparison_fn_t compare  //!< Comparison function 
+){
+}
+#endif
+
+
+  
+/*! 
+ * @addtogroup CVSLOG CVSLOG
+ *  $Log: tk_ansi_search.c,v $
+ *  Revision 1.1  2006-02-09 22:40:39  ambrmi09
+ *  Initial creation of ANSI re-implementation module
+ *
+ *  
+ *******************************************************************/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
