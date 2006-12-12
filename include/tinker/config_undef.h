92269a7379a48abf83c14339294da2709b0fb5a5
ambrmi09 <ambrmi09>
2006-12-12 11:36:00 +0000

    minor

---
+++ include/tinker/config_undef.h
@@ -0,0 +1,6 @@
+//This file contains undefines of macros that can interfere with userspace
+//They should have been redefined previously in config.h
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
