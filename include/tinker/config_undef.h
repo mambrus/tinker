b54c8e866729078bc444f2abe24de3722b6c8159
Michael Ambrus <michael.ambrus@gmail.com>
2014-06-13 21:08:22 +0200

    BUILDCHAIN wrapper issue fixed

--- include/tinker/config_undef.h
+++ include/tinker/config_undef.h
@@ -1,6 +1,14 @@
-//This file contains undefines of macros that can interfere with userspace
-//They should have been redefined previously in config.h
+/* This file contains undefines of macros that can interfere with userspace
+   They should have been redefined previously in config.h */
 #undef PACKAGE_BUGREPORT
 #undef PACKAGE_NAME
 #undef PACKAGE_STRING
 #undef PACKAGE_TARNAME
+
+
+/* For each define that can interfere with the BUILDCHAIN:s macros undefine
+   them but save their settings just in case in WAS_DEFINED_xxx */
+#ifdef linux
+#  define WAS_DEFINED_linux linux
+#  undef linux
+#endif
